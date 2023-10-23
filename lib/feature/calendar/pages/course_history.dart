538b0c2a4eacb3ce8bda4ef075b5ac2ff52cfcfe
pisanuwat <pisanuwat.b@gmail.com>
2023-10-23 08:52:51 +0700

    -fix: prevent mobile tutor to access review page

--- lib/feature/calendar/pages/course_history.dart
+++ lib/feature/calendar/pages/course_history.dart
@@ -14,6 +14,7 @@ import 'package:solve_tutor/feature/calendar/widgets/sizebox.dart';
 
 import '../../../authentication/service/auth_provider.dart';
 import '../../live_classroom/page/review_lesson.dart';
+import '../../live_classroom/utils/responsive.dart';
 import '../widgets/format_date.dart';
 import '../widgets/sizebox.dart';
 
@@ -180,6 +181,7 @@ class _CourseHistoryState extends State<CourseHistory>
 
                 return InkWell(
                   onTap: () {
+                    if (Responsive.isMobile(context)) return;
                     Navigator.push(
                       context,
                       MaterialPageRoute(
