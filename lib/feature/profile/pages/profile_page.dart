9deed0558c94ff6246a1ffb8b98316c79c8575a4
pisanuwat <pisanuwat.b@gmail.com>
2023-11-04 16:40:31 +0700

    -config: auto-generated window build + change profile version

--- lib/feature/profile/pages/profile_page.dart
+++ lib/feature/profile/pages/profile_page.dart
@@ -454,7 +454,7 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                         const SizedBox(height: 10),
                         const Text(
-                          'SOLVE Instructor v 0.3.62',
+                          'SOLVE Instructor v 0.3.7',
                           style: TextStyle(
                             color: greyColor,
                           ),
