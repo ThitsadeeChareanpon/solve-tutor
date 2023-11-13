9c1303b59c4e56dcd8468608a912bac47c3a60ce
pisanuwat <pisanuwat.b@gmail.com>
2023-11-13 17:22:13 +0700

    -change: update profile version

--- lib/feature/profile/pages/profile_page.dart
+++ lib/feature/profile/pages/profile_page.dart
@@ -454,7 +454,7 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                         const SizedBox(height: 10),
                         const Text(
-                          'SOLVE Instructor v 0.3.83',
+                          'SOLVE Instructor v 0.4.0',
                           style: TextStyle(
                             color: greyColor,
                           ),
