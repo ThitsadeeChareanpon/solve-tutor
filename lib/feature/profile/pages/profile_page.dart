d4a874362567e10d8bb237c30aba517129f9acce
pisanuwat <pisanuwat.b@gmail.com>
2023-11-17 18:23:44 +0700

    -release-V3: stable marketplace & hybrid

--- lib/feature/profile/pages/profile_page.dart
+++ lib/feature/profile/pages/profile_page.dart
@@ -454,7 +454,7 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                         const SizedBox(height: 10),
                         const Text(
-                          'SOLVE Instructor v 0.4.11',
+                          'SOLVE Instructor v 0.4.2',
                           style: TextStyle(
                             color: greyColor,
                           ),
